42d253edd3873ab8b3538958d2659df732c3aecd
Kyle B <kberry1298@gmail.com>
2023-12-09 10:24:37 -0600

    got authentication working, at a good stopping point

--- levelupapi/models/gamer.py
+++ levelupapi/models/gamer.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Gamer(models.Model):
 
-    uid = models.CharField(max_length=55)
+    uid = models.CharField(max_length=55, unique=True)
     bio = models.CharField(max_length=50)
